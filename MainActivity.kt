50a72721869a9e4692ea31a0b6b6ae091c3f81bb
Dmitriy Chebotar <optimussflash@ukr.net>
2020-03-15 17:02:57 +0200

    initial commit

---
+++ MainActivity.kt
@@ -0,0 +1,15 @@
+package com.optimus.simplestopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.concurrent.fixedRateTimer
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+    }
+}
